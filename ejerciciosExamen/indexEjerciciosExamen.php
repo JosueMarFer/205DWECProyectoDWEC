30a1ddf9bde2dbfd9f08c900bc3fa496305b43e5
ZenOne <ZenOne@DESKTOP-J2J3KAD>
2023-02-21 23:35:32 +0100

    21/02/2023
    Ejercicios añadidos

---
+++ ejerciciosExamen/indexEjerciciosExamen.php
@@ -0,0 +1,90 @@
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>205DWECEjercicios9</title>
+    <link rel="stylesheet" href="../webroot/css/style.css" />
+  </head>
+
+  <body>
+    <header>
+      <h1>&lt;/DWEC&gt;</h1>
+      <h2>Proyecto DWEC Ejercicios 9</h2>
+    </header>
+    <main>
+      <h3>Enunciado</h3>
+      <table class="tabla">
+        <thead>
+          <tr>
+            <th>Elemento</th>
+            <th>Mostrar</th>
+          </tr>
+          <tr class="filaVacia">
+            <td></td>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>
+              Enunciado
+            </td>
+            <td>
+              <a href="./doc/enunciado.pdf" target="_blank">
+                <img src="../webroot/images/mostrarCodigo.png" alt="Mostrar Codigo" />
+              </a>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+      <h3>Ejercicios</h3>
+      <table class="tabla">
+        <thead>
+          <tr>
+            <th>Ejercicio</th>
+            <th>Ejecutar código</th>
+            <th>Mostrar código</th>
+          </tr>
+          <tr class="filaVacia">
+            <td></td>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>
+              Ejercicio Examen.
+            </td>
+            <td>
+              <a target="_blank" href="./codigoJS/ejercicioExamen.html">
+                <img src="../webroot/images/ejecutar.png" alt="Ejecutar codigo" />
+              </a>
+            </td>
+            <td>
+              <a href="./mostrarcodigo/muestraEjercicioExamen.php">
+                <img src="../webroot/images/mostrarCodigo.png" alt="Mostrar Codigo" />
+              </a>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+      <div class="return">
+        <a href="../indexProyectoDWEC.php"><img src="../webroot/images/back.png" alt="Imagen back"></a>
+      </div>
+    </main>
+    <footer>
+      <div class="footerIcons">
+        <a href="../doc/curriculum.pdf" target="_blank"><img src="../webroot/images/curriculum.png"
+                                                             alt="Imagen curriculum"></a>
+        <a href="https://github.com/JosueMarFer/205DWECProyectoDWEC.php" target="_blank"><img
+            src="../webroot/images/github.png" alt="Imagen github"></a>
+      </div>
+      <div class="home">
+        <a href="../../index.html"><img src="../webroot/images/home.png" alt="Imagen home"></a>
+        <p>Josué martínez Fernández</p>
+      </div>
+    </footer>
+  </body>
+
+</html>
